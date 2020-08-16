241aa66d65673c8f0a3a40ca6dc8d27554ce1355
IT19111148 <jayasinghenavodi@gmail.com>
2020-08-17 03:21:35 +0530

    User commit

---
+++ app/src/main/java/com/example/eshoppingmanagementsytemuser/AddingProducts.java
@@ -0,0 +1,14 @@
+package com.example.eshoppingmanagementsytemuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_products);
+    }
+}
